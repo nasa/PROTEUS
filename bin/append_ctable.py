ae27b34d3381ae10b088fb9877c2b7565e8e3f5e
Gustavo Shiroma <gustavo.h.shiroma@jpl.nasa.gov>
2021-12-08 15:04:13 -0800

    remove license from the beginning of the Python scripts

--- bin/append_ctable.py
+++ bin/append_ctable.py
@@ -1,33 +1,3 @@
-#!/usr/bin/env python3
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#
-# Compute Dynamic Surface Water Extent (DSWx)
-# 
-# OPERA
-#
-# Copyright 2021, by the California Institute of Technology. ALL RIGHTS
-# RESERVED. United States Government Sponsorship acknowledged.
-# Any commercial use must be negotiated with the Office of Technology Transfer
-# at the California Institute of Technology.
-#
-# This software may be subject to U.S. export control laws. By accepting this
-# software, the user agrees to comply with all applicable U.S.
-# export laws and regulations. User has the responsibility to obtain export
-# licenses, or other export authority as may be required before exporting such
-# information to foreign countries or providing access to foreign persons.
-#
-# 
-# References:
-# [1] Jones, J. W. (2015). Efficient wetland surface water detection and 
-# monitoring via Landsat: Comparison with in situ data from the Everglades 
-# Depth Estimation Network. Remote Sensing, 7(9), 12503-12538. 
-# http://dx.doi.org/10.3390/rs70912503.
-# 
-# [2] R. Dittmeier, LANDSAT DYNAMIC SURFACE WATER EXTENT (DSWE) ALGORITHM 
-# DESCRIPTION DOCUMENT (ADD)", USGS, March 2018
-#
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 import argparse
 from osgeo import gdal
 from dswx_hls import save_mask, save_dswx_product, band_description_dict
