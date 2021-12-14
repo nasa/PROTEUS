38bad0b855c3e3a167455a1ee7efbf334cda1e75
Gustavo Shiroma <gustavo.h.shiroma@jpl.nasa.gov>
2021-12-13 16:27:52 -0800

    move dictionaries outside DSWx-HLS main script

--- bin/dswx_hls.py
+++ bin/dswx_hls.py
@@ -27,6 +27,15 @@ from ruamel.yaml import YAML as ruamel_yaml
 from osgeo.gdalconst import GDT_Float32
 from scipy.ndimage import convolve
 from osgeo import gdal, osr
+
+from modules import l30_v1_band_dict, \
+                             s30_v1_band_dict, \
+                             l30_v2_band_dict, \
+                             s30_v2_band_dict, \
+                             interpreted_dswx_band_dict, \
+                             band_description_dict, \
+                             METADATA_FIELDS_TO_COPY_FROM_HLS_LIST
+
 # from memory_profiler import profile
  
 logger = logging.getLogger('dswx_hls')
@@ -66,101 +75,6 @@ version = True  # Displays the DSWE application version number.
 
 FLAG_APPLY_MASK_INVALID_IND = True
 
-l30_v1_band_dict = {'blue': 'band02',
-                    'green': 'band03',
-                    'red': 'band04',
-                    'nir': 'band05',
-                    'swir1': 'band06',
-                    'swir2': 'band07',
-                    'qa': 'QA'}
-
-s30_v1_band_dict = {'blue': 'band02',
-                    'green': 'band03',
-                    'red': 'band04',
-                    'nir': 'band8A',
-                    'swir1': 'band11',
-                    'swir2': 'band12',
-                    'qa': 'QA'}
-
-l30_v2_band_dict = {'blue': 'B02',
-                    'green': 'B03',
-                    'red': 'B04',
-                    'nir': 'B05',
-                    'swir1': 'B06',
-                    'swir2': 'B07',
-                    'qa': 'Fmask'}
-
-s30_v2_band_dict = {'blue': 'B02',
-                    'green': 'B03',
-                    'red': 'B04',
-                    'nir': 'B8A',
-                    'swir1': 'B11',
-                    'swir2': 'B12',
-                    'qa': 'Fmask'}
-
-interpreted_dswx_band_dict = {
-    0b00000 : 0,  # (Not Water)
-    0b00001 : 0,
-    0b00010 : 0,
-    0b00100 : 0,
-    0b01000 : 0,
-    0b01111 : 1,  # (Water - High Confidence)
-    0b10111 : 1,
-    0b11011 : 1,
-    0b11101 : 1,
-    0b11110 : 1,
-    0b11111 : 1,
-    0b00111 : 2,  # (Water - Moderate Confidence)
-    0b01011 : 2,
-    0b01101 : 2,
-    0b01110 : 2,
-    0b10011 : 2,
-    0b10101 : 2,
-    0b10110 : 2,
-    0b11001 : 2,
-    0b11010 : 2,
-    0b11100 : 2,
-    0b11000 : 3,  # (Potential Wetland)
-    0b00011 : 4,  #(Low Confidence Water or Wetland)
-    0b00101 : 4,
-    0b00110 : 4,
-    0b01001 : 4,
-    0b01010 : 4,
-    0b01100 : 4,
-    0b10000 : 4,
-    0b10001 : 4,
-    0b10010 : 4,
-    0b10100 : 4}
-    # 0b255 :  # 255 (Fill)
-
-band_description_dict = {
-    'WTR': 'Water classification (WTR)',
-    'BWTR': 'Binary Water (BWTR)',
-    'CONF': 'TBD Confidence (CONF)',
-    'DIAG': 'Diagnostic layer (DIAG)',
-    'INTR': 'Interpretation of diagnostic layer into water classes (INTR)',
-    'INSM': 'Interpreted layer refined using land cover and terrain shadow testing (INSM)',
-    'LAND': 'Land cover classification (LAND)',
-    'SHAD': 'Terrain shadow layer (SHAD)',
-    'CLOUD': 'Cloud/cloud-shadow classification (CLOUD)',
-    'DEM': 'Digital elevation model (DEM)'}
-
-
-METADATA_FIELDS_TO_COPY_FROM_HLS_LIST = ['SENSOR_PRODUCT_ID',
-                                         'SENSING_TIME',
-                                         'SPATIAL_COVERAGE',
-                                         'CLOUD_COVERAGE',
-                                         'SPATIAL_RESAMPLING_ALG',
-                                         # 'HORIZONTAL_CS_NAME',
-                                         # 'ULX',
-                                         # 'ULY',
-                                         'MEAN_SUN_AZIMUTH_ANGLE',
-                                         'MEAN_SUN_ZENITH_ANGLE',
-                                         'MEAN_VIEW_AZIMUTH_ANGLE',
-                                         'MEAN_VIEW_ZENITH_ANGLE',
-                                         'NBAR_SOLAR_ZENITH',
-                                         'ACCODE',
-                                         'IDENTIFIER_PRODUCT_DOI']
 
 def _get_parser():
     parser = argparse.ArgumentParser(
