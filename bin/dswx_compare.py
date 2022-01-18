c1afad540dff326585ea55fde99b0e0e7bc5936a
Gustavo Shiroma <gustavo.h.shiroma@jpl.nasa.gov>
2022-01-18 15:12:06 -0800

    remove copyright from bin/dswx_compare.py

--- bin/dswx_compare.py
+++ bin/dswx_compare.py
@@ -1,22 +1,6 @@
 #!/usr/bin/env python3
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
-# Compute Dynamic Surface Water Extent (DSWx)
-# 
-# OPERA
-#
-# Copyright 2021, by the California Institute of Technology. ALL RIGHTS
-# RESERVED. United States Government Sponsorship acknowledged.
-# Any commercial use must be negotiated with the Office of Technology Transfer
-# at the California Institute of Technology.
-#
-# This software may be subject to U.S. export control laws. By accepting this
-# software, the user agrees to comply with all applicable U.S.
-# export laws and regulations. User has the responsibility to obtain export
-# licenses, or other export authority as may be required before exporting such
-# information to foreign countries or providing access to foreign persons.
-#
-# 
 # References:
 # [1] J. W. Jones, "Efficient wetland surface water detection and 
 # monitoring via Landsat: Comparison with in situ data from the Everglades 
