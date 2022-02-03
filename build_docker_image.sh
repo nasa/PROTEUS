b11a3fc8618145c0f0cbb6214d9fdf7dd2d68ff9
Gustavo Shiroma <gustavo.h.shiroma@jpl.nasa.gov>
2022-02-03 14:13:47 -0800

    add Docker files and scripts

---
+++ build_docker_image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IMAGE=opera/dswx_hls
+t=interface
+echo "IMAGE is $IMAGE:$t"
+
+# fail on any non-zero exit codes
+set -ex
+
+python3 setup.py sdist
+
+# build image
+docker build --rm --force-rm --network=host -t ${IMAGE}:$t -f docker/Dockerfile .
+
+# create image tar
+docker save opera/dswx_hls > docker/dockerimg_dswx_hls_$t.tar
+
+# remove image
+docker image rm opera/dswx_hls:$t
