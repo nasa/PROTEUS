a0fa992e9552e1487c570657b0fede1130be5ba4
Ryan Burns <rtburns@jpl.nasa.gov>
2022-05-16 14:45:48 -0700

    Switch CircleCI from docker executor to machine executor (#19)
    * Switch CircleCI from docker executor to machine executor
    
    The docker executor does not support docker volume mounting within
    
    * Remove setup_remote_docker command
    
    This is specific to the docker executor
    
    * Become superuser for apt-get commands
    
    * Re-attempt pytest command using mounted test directory
    
    * Split dependency installation from docker build

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -12,6 +12,9 @@ python3 setup.py sdist
 # build image
 docker build --rm --force-rm --network=host -t ${IMAGE}:$t -f docker/Dockerfile .
 
+ # run tests
+docker run --rm -u "$(id -u):$(id -g)" -v "$PWD:/mnt" -w /mnt -it "${IMAGE}:$t" pytest /mnt/tests/
+
 # create image tar
 docker save opera/proteus > docker/dockerimg_proteus_$t.tar
 
