fe5f813a0c1cd32afc7a976094fd03839a657a90
Gustavo Shiroma <gustavo.h.shiroma@jpl.nasa.gov>
2022-12-21 22:38:33 -0800

    update _get_version() per Sam's suggestion

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import re
 from setuptools import setup
 from setuptools import Command
 
@@ -13,13 +14,28 @@ def _get_version():
     """
 
     version_file = 'src/proteus/version.py'
-    version = open(version_file).read().split('=')[1]
-    version = version.replace("'", '').replace('"', '').strip()
-    
-    return version
+    with open(version_file, 'r') as f:
+        text = f.read()
+
+    # Get first match of the version number contained in the version file
+    # This regex should match a pattern like: VERSION = '3.2.5', but it
+    # allows for varying spaces, number of major/minor versions,
+    # and quotation mark styles.
+    p = re.search("VERSION[ ]*=[ ]*['\"]\d+([.]\d+)*['\"]", text)
+
+    # Check that the version file contains properly formatted text string
+    if p is None:
+        raise ValueError(f'Version file {version_file} not properly formatted.'
+                     " It should contain text matching e.g. VERSION = '2.3.4'")
+
+    # Extract just the numeric version number from the string
+    p = re.search("\d+([.]\d+)*", p.group(0))
+
+    return p.group(0)
 
 __version__ = version = VERSION = _get_version()
 
+print(f'proteus version {version}')
 
 class CleanCommand(Command):
     """Custom clean command to tidy up the project root 
