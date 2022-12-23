16ca9deebb2ae1f184b9fe8580df8deafdd53034
Gustavo Shiroma <gustavo.h.shiroma@jpl.nasa.gov>
2022-12-22 17:34:59 -0800

    update comments for the diagnostic tests

--- src/proteus/dswx_hls.py
+++ src/proteus/dswx_hls.py
@@ -81,18 +81,24 @@ DIAGNOSTIC_LAYER_NO_DATA_DECIMAL = 0b100000
 DIAGNOSTIC_LAYER_NO_DATA_BINARY_REPR = 65535
 
 interpreted_dswx_band_dict = {
-    0b00000 : 0,  # (Not Water)
+
+    # Not water
+    0b00000 : 0,
     0b00001 : 0,
     0b00010 : 0,
     0b00100 : 0,
     0b01000 : 0,
-    0b01111 : 1,  # (Water - High Confidence)
+
+    # Water - high confidence
+    0b01111 : 1,
     0b10111 : 1,
     0b11011 : 1,
     0b11101 : 1,
     0b11110 : 1,
     0b11111 : 1,
-    0b00111 : 2,  # (Water - Moderate Confidence)
+
+    # Water - Moderate confidence
+    0b00111 : 2,
     0b01011 : 2,
     0b01101 : 2,
     0b01110 : 2,
@@ -102,8 +108,12 @@ interpreted_dswx_band_dict = {
     0b11001 : 2,
     0b11010 : 2,
     0b11100 : 2,
-    0b11000 : 3,  # (Potential Wetland)
-    0b00011 : 4,  #(Low Confidence Water or Wetland)
+
+    # Partial surface water conservative (previous potential wetland)
+    0b11000 : 3,
+
+    # Partial surface water aggressive (previous low confidence water or wetland))
+    0b00011 : 4,
     0b00101 : 4,
     0b00110 : 4,
     0b01001 : 4,
@@ -113,6 +123,8 @@ interpreted_dswx_band_dict = {
     0b10001 : 4,
     0b10010 : 4,
     0b10100 : 4,
+
+    # Fill value
     DIAGNOSTIC_LAYER_NO_DATA_DECIMAL: UINT8_FILL_VALUE
 }
 
