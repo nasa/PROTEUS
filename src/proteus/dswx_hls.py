33c00a2680540db2a26e039442cc2c4f84cf4b05
Gustavo H. X. Shiroma <52007211+gshiroma@users.noreply.github.com>
2023-03-08 16:56:35 -0800

    Add support to antimeridian ("dateline") crossing (#43)
    * add processing parameters to DSWx-HLS metadata
    
    * merge main into final_release
    
    * add processing parameters to DSWx-HLS metadata (2)
    
    * rename metadata fields SPATIAL_COVERAGE and CLOUD_COVERAGE to INPUT_HLS_PRODUCT_SPATIAL_COVERAGE and INPUT_HLS_PRODUCT_CLOUD_COVERAGE, respectivelly
    
    * compute SPATIAL_COVERAGE and CLOUD_COVERAGE
    
    * move _check_ancillary_inputs() before first use of the shoreline shapefile
    
    * rename _compute_and_apply_cloud_layer() to _add_snow_to_cloud_layer_and_apply_cloud_masking() and update docstrings of functions _compute_and_apply_cloud_layer() and _add_snow_to_cloud_layer_and_apply_cloud_masking()
    
    * remove `image_dict` when it's no longer needed
    
    * compute CLOUD_COVERAGE over valid pixels (rather than total number of pixels)
    
    * only assign a value to the OCEAN_MASKING_SHORELINE_DISTANCE_KM metadata field if a shoreline shapefile is provided
    
    * add no data value to LAND layer
    
    * fix bug introduced in last commits: copy wtr_2 layer to wtr rather than initializing it with zeros
    
    * add metadata field SPATIAL_COVERAGE_AFTER_OCEAN_MASKING
    
    * fix docstrings for function _compute_opera_shadow_layer()
    
    * assign "-" rather than "" to empty FOREST_MASK_LANDCOVER_CLASSES
    
    * update _compute_preliminary_cloud_layer() and _add_snow_to_cloud_layer() docstrings
    
    * update docstrings for _add_snow_to_cloud_layer(
    
    * remove unnecessary del
    
    * update docstrings
    
    * update docstrings for _compute_preliminary_cloud_layer()
    
    * update docstrings _add_snow_to_cloud_layer()
    
    * add high-aerosol masking
    
    * add flag to enable/disable aerosol masking
    
    * fix aerosol masking logic
    
    * partial surface water becomes moderate conf. water when aerosol masked
    
    * move assignment of `n_not_ocean` near function _create_ocean_mask() call
    
    * not water becomes moderate-conf water when aerosol masked
    
    * update aerosol masking variable names and docstrings
    
    * update CLOUD layer, variables, and docstrings after adding aerosol class remapping
    
    * fix typo when apply_aerosol_class_remapping is not previously set
    
    * fix aerosol remapping and assignment of the ocre color
    
    * do not apply CLOUD masking to high aerosol areas (value 8)
    
    * improve logic for updating the CLOUD layer with high-aerosol pixels
    
    * update color of aeresol reassined classes from ocre to a grayish blue
    
    * revert aeresol reassined classes color to ocre
    
    * revert color changes for the moderate conf. water layer
    
    * add flag to enable/disable the ocean masking
    
    * update moderate conf. water class color
    
    * add flag to enable/disable the ocean masking (2)
    
    * Update _warp function to handle the dateline crossing
    
    * Update _warp function to handle the dateline crossing (2)
    
    * improve variable names; add margin in degrees; improve messages to the user
    
    * instead of adding a margin in degree, increase existing margin in meters; remove longitude 360 deg shifting
    
    * improve log messages

--- src/proteus/dswx_hls.py
+++ src/proteus/dswx_hls.py
@@ -952,8 +952,8 @@ def create_landcover_mask(copernicus_landcover_file,
         dir=scratch_dir, suffix='.tif').name
 
     copernicus_landcover_array = _warp(copernicus_landcover_file,
-        geotransform, projection,
-        length, width, scratch_dir, resample_algorithm='nearest',
+        geotransform, projection, length, width,
+        scratch_dir, resample_algorithm='nearest',
         relocated_file=copernicus_landcover_reprojected_file,
         temp_files_list=temp_files_list)
     temp_files_list.append(copernicus_landcover_reprojected_file)
@@ -967,8 +967,8 @@ def create_landcover_mask(copernicus_landcover_file,
     worldcover_reprojected_up_3_file = tempfile.NamedTemporaryFile(
         dir=scratch_dir, suffix='.tif').name
     worldcover_array_up_3 = _warp(worldcover_file, geotransform_up_3,
-        projection, length_up_3, width_up_3, scratch_dir,
-        resample_algorithm='nearest',
+        projection, length_up_3, width_up_3,
+        scratch_dir, resample_algorithm='nearest',
         relocated_file=worldcover_reprojected_up_3_file,
         temp_files_list=temp_files_list)
     temp_files_list.append(worldcover_reprojected_up_3_file)
@@ -1932,7 +1932,6 @@ def _compute_preliminary_cloud_layer(fmask, mask_adjacent_to_cloud_mode):
     preliminary_cloud_layer[np.bitwise_and(fmask, 2**1) == 2**1] += 4
 
     return preliminary_cloud_layer
-
 
 
 def _add_snow_to_cloud_layer(wtr_2_layer, cloud_layer, fmask,
@@ -3089,10 +3088,11 @@ def get_projection_proj4(projection):
 
 
 def _warp(input_file, geotransform, projection,
-              length, width, scratch_dir = '.',
-              resample_algorithm='nearest',
-              relocated_file=None, margin_in_pixels=0,
-              temp_files_list = None):
+          length, width,
+          scratch_dir = '.',
+          resample_algorithm='nearest',
+          relocated_file=None, margin_in_pixels=0,
+          temp_files_list = None):
     """Relocate/reproject a file (e.g., landcover or DEM) based on geolocation
        defined by a geotransform, output dimensions (length and width)
        and projection
@@ -3135,36 +3135,137 @@ def _warp(input_file, geotransform, projection,
     dx = geotransform[1]
 
     # Output Y-coordinate start (North) position with margin
-    y0 = geotransform[3] - margin_in_pixels * dy
+    tile_max_y_utm = geotransform[3] - margin_in_pixels * dy
 
     # Output X-coordinate start (West) position with margin
-    x0 = geotransform[0] - margin_in_pixels * dx
+    tile_min_x_utm = geotransform[0] - margin_in_pixels * dx
 
     # Output Y-coordinate end (South) position with margin
-    yf = y0 + (length + 2 * margin_in_pixels) * dy
+    tile_min_y_utm = tile_max_y_utm + (length + 2 * margin_in_pixels) * dy
 
     # Output X-coordinate end (East) position with margin
-    xf = x0 + (width + 2 * margin_in_pixels) * dx
+    tile_max_x_utm = tile_min_x_utm + (width + 2 * margin_in_pixels) * dx
 
     # Set output spatial reference system (SRS) from projection
-    dstSRS = get_projection_proj4(projection)
+    tile_srs_str = get_projection_proj4(projection)
+    tile_srs = osr.SpatialReference()
+    tile_srs.ImportFromProj4(projection)
 
     if relocated_file is None:
         relocated_file = tempfile.NamedTemporaryFile(
                     dir=scratch_dir, suffix='.tif').name
-        logger.info(f'    relocating file: {input_file} to'
-                    f' temporary file: {relocated_file}')
         if temp_files_list is not None:
             temp_files_list.append(relocated_file)
-    else:
-        logger.info(f'    relocating file: {input_file} to'
-                    f' file: {relocated_file}')
 
     _makedirs(relocated_file)
 
-    gdal.Warp(relocated_file, input_file, format='GTiff',
-              dstSRS=dstSRS,
-              outputBounds=[x0, yf, xf, y0], multithread=True,
+    # Test for antimeridian ("dateline") crossing
+    gdal_ds = gdal.Open(input_file, gdal.GA_ReadOnly)
+    file_projection = gdal_ds.GetProjection()
+    file_srs = osr.SpatialReference()
+    file_srs.ImportFromProj4(file_projection)
+
+    # margin 5km
+    margin_m = 5000
+
+    tile_polygon, tile_min_y, tile_max_y, tile_min_x, tile_max_x = \
+        _get_tile_srs_bbox(tile_min_y_utm - margin_m,
+                           tile_max_y_utm + margin_m,
+                           tile_min_x_utm - margin_m,
+                           tile_max_x_utm + margin_m,
+                           tile_srs, file_srs)
+
+    # if it doesn't cross the antimeridian, reproject
+    # input file using gdalwarp
+    if not (file_srs.IsGeographic() and tile_min_x < 180 and
+            tile_max_x >= 180):
+
+        logger.info(f'    relocating file: {input_file} to'
+                    f' file: {relocated_file}')
+
+        gdal.Warp(relocated_file, input_file,
+                  format='GTiff',
+                  dstSRS=tile_srs_str,
+                  outputBounds=[tile_min_x_utm, tile_min_y_utm,
+                                tile_max_x_utm, tile_max_y_utm],
+                  multithread=True,
+                  xRes=dx, yRes=abs(dy), resampleAlg=resample_algorithm,
+                  errorThreshold=0)
+
+        gdal_ds = gdal.Open(relocated_file, gdal.GA_ReadOnly)
+        relocated_array = gdal_ds.ReadAsArray()
+        del gdal_ds
+
+        return relocated_array
+
+    logger.info(f'    tile crosses the antimeridian')
+
+    file_geotransform = gdal_ds.GetGeoTransform()
+    file_min_x, file_dx, _, file_max_y, _, file_dy = file_geotransform
+
+    file_width = gdal_ds.GetRasterBand(1).XSize
+    file_length = gdal_ds.GetRasterBand(1).YSize
+    no_data = gdal_ds.GetRasterBand(1).GetNoDataValue()
+
+    del gdal_ds
+
+    file_max_x = file_min_x + file_width * file_dx
+    file_min_y = file_max_y + file_length * file_dy
+
+    # Crop input at the two sides of the antimeridian:
+    # left side: use tile bbox with file_max_x from input
+    proj_win_antimeridian_left = [tile_min_x,
+                                  tile_max_y,
+                                  file_max_x,
+                                  tile_min_y]
+
+    cropped_input_antimeridian_left_temp = tempfile.NamedTemporaryFile(
+                dir=scratch_dir, suffix='.tif').name
+    logger.info(f'    cropping antimeridian-left side: {input_file} to'
+                f' temporary file: {cropped_input_antimeridian_left_temp}'
+                ' with indexes (ulx uly lrx lry):'
+                f'{proj_win_antimeridian_left}')
+
+    gdal.Translate(cropped_input_antimeridian_left_temp, input_file,
+                   projWin=proj_win_antimeridian_left,
+                   outputSRS=file_srs,
+                   noData=no_data)
+
+    # right side: use tile bbox with file_min_x from input and tile_max_x
+    # with subtracted 360 (lon) degrees
+    proj_win_antimeridian_right = [file_min_x,
+                                   tile_max_y,
+                                   tile_max_x - 360,
+                                   tile_min_y]
+    cropped_input_antimeridian_right_temp = tempfile.NamedTemporaryFile(
+                dir=scratch_dir, suffix='.tif').name
+
+    logger.info(f'    cropping antimeridian-right side: {input_file} to'
+                f' temporary file: {cropped_input_antimeridian_right_temp}'
+                ' with indexes (ulx uly lrx lry):'
+                f'{proj_win_antimeridian_right}')
+
+    gdal.Translate(cropped_input_antimeridian_right_temp, input_file,
+                   projWin=proj_win_antimeridian_right,
+                   outputSRS=file_srs,
+                   noData=no_data)
+
+    if temp_files_list is not None:
+        temp_files_list.append(cropped_input_antimeridian_left_temp)
+        temp_files_list.append(cropped_input_antimeridian_right_temp)
+
+    gdalwarp_input_file_list = [cropped_input_antimeridian_left_temp,
+                                cropped_input_antimeridian_right_temp]
+
+    logger.info(f'    relocating file: {input_file} to'
+                f' file: {relocated_file}')
+
+    gdal.Warp(relocated_file, gdalwarp_input_file_list,
+              format='GTiff',
+              dstSRS=tile_srs_str,
+              outputBounds=[tile_min_x_utm, tile_min_y_utm,
+                            tile_max_x_utm, tile_max_y_utm],
+              multithread=True,
               xRes=dx, yRes=abs(dy), resampleAlg=resample_algorithm,
               errorThreshold=0)
 
@@ -3237,7 +3338,7 @@ def _get_tile_srs_bbox(tile_min_y_utm, tile_max_y_utm,
     tile_min_x = np.min(tile_x_array)
     tile_max_x = np.max(tile_x_array)
 
-    # handles anti-meridian: tile_max_x around +180 and tile_min_x around -180
+    # handles antimeridian: tile_max_x around +180 and tile_min_x around -180
     # add 360 to tile_min_x, so it becomes a little greater than +180
     if tile_max_x > tile_min_x + 340:
         tile_min_x, tile_max_x = tile_max_x, tile_min_x + 360
@@ -3286,7 +3387,7 @@ def _create_ocean_mask(shapefile, margin_km, scratch_dir,
        ocean_mask : numpy.ndarray
               Ocean mask (1: land, 0: ocean)
     """
-    logger.info('creating ocean mask')
+    logger.info('creating the ocean mask')
 
     tile_min_x_utm, tile_dx_utm, _, tile_max_y_utm, _, tile_dy_utm = \
         geotransform
@@ -4168,26 +4269,61 @@ def _check_ancillary_inputs(check_ancillary_inputs_coverage,
                                tile_srs, file_srs)
 
         # Create input ancillary polygon
-        file_ring = ogr.Geometry(ogr.wkbLinearRing)
-        file_ring.AddPoint(min_x, max_y)
-        file_ring.AddPoint(max_x, max_y)
-        file_ring.AddPoint(max_x, min_y)
-        file_ring.AddPoint(min_x, min_y)
-        file_ring.AddPoint(min_x, max_y)
-        file_polygon = ogr.Geometry(ogr.wkbPolygon)
-        file_polygon.AddGeometry(file_ring)
-        file_polygon.AssignSpatialReference(file_srs)
-        assert file_polygon.IsValid()
+        file_polygon = _get_ogr_polygon(min_x, max_y, max_x, min_y, file_srs)
 
-        if not tile_polygon.Within(file_polygon):
-            error_msg = f'ERROR the {file_description} with extents'
-            error_msg += f' S/N: [{min_y},{max_y}]'
-            error_msg += f' W/E: [{min_x},{max_x}],'
-            error_msg += ' does not fully cover input tile with'
-            error_msg += f' extents S/N: [{tile_min_y},{tile_max_y}]'
-            error_msg += f' W/E: [{tile_min_x},{tile_max_x}]'
-            logger.error(error_msg)
-            raise ValueError(error_msg)
+        if tile_polygon.Within(file_polygon):
+            continue
+
+        # Handle antimeridian ("dateline") crossing
+        if file_srs.IsGeographic() and tile_min_x < 180 and tile_max_x >= 180:
+
+            logger.info(f'The input HLS product crosses the antimeridian'
+                        ' (dateline). Verifying the'
+                        f' {file_description}: {file_name}')
+
+            # Left side of the antimeridian crossing: -180 -> +180
+            file_polygon_1 = _get_ogr_polygon(-180, 90, max_x, -90, file_srs)
+            intersection_1 = tile_polygon.Intersection(file_polygon_1)
+            flag_1_ok = intersection_1.Within(file_polygon)
+            check_1_str = 'ok' if flag_1_ok else 'fail'
+            logger.info(f'    left side (-180 -> +180): {check_1_str}')
+
+            # Right side of the antimeridian crossing: +180 -> +360
+            buffer_in_degrees = 0.0002777  # buffer of 1 arcsec: ~ 30m
+            file_polygon_2 = _get_ogr_polygon(
+                max_x + buffer_in_degrees, 90, max_x + 360, -90, file_srs)
+            intersection_2 = tile_polygon.Intersection(file_polygon_2)
+            file_polygon_2 = _get_ogr_polygon(min_x + 360, max_y,
+                                                    max_x + 360, min_y,
+                                                    file_srs)
+            flag_2_ok = intersection_2.Within(file_polygon_2)
+            check_2_str = 'ok' if flag_2_ok else 'fail'
+            logger.info(f'    right side (+180 -> +360): {check_2_str}')
+
+            if flag_1_ok and flag_2_ok:
+                continue
+
+        error_msg = f'ERROR the {file_description} with extents'
+        error_msg += f' S/N: [{min_y},{max_y}]'
+        error_msg += f' W/E: [{min_x},{max_x}],'
+        error_msg += ' does not fully cover input tile with'
+        error_msg += f' extents S/N: [{tile_min_y},{tile_max_y}]'
+        error_msg += f' W/E: [{tile_min_x},{tile_max_x}]'
+        logger.error(error_msg)
+        raise ValueError(error_msg)
+
+def _get_ogr_polygon(min_x, max_y, max_x, min_y, file_srs):
+    file_ring = ogr.Geometry(ogr.wkbLinearRing)
+    file_ring.AddPoint(min_x, max_y)
+    file_ring.AddPoint(max_x, max_y)
+    file_ring.AddPoint(max_x, min_y)
+    file_ring.AddPoint(min_x, min_y)
+    file_ring.AddPoint(min_x, max_y)
+    file_polygon = ogr.Geometry(ogr.wkbPolygon)
+    file_polygon.AddGeometry(file_ring)
+    file_polygon.AssignSpatialReference(file_srs)
+    assert file_polygon.IsValid()
+    return file_polygon
 
 
 def generate_dswx_layers(input_list,
@@ -4722,8 +4858,8 @@ def generate_dswx_layers(input_list,
             temp_files_list.append(dem_cropped_file)
         logger.info(f'Preparing DEM file: {dem_file}')
         dem_with_margin = _warp(dem_file, geotransform, projection,
-                                    length, width, scratch_dir,
-                                    resample_algorithm='cubic',
+                                    length, width,
+                                    scratch_dir, resample_algorithm='cubic',
                                     relocated_file=dem_cropped_file,
                                     margin_in_pixels=DEM_MARGIN_IN_PIXELS,
                                     temp_files_list=temp_files_list)
