6cb0c2090e1841744bcf5735fd3fbf1b1d1eaf49
Charlie Marshak <cmarshak@users.noreply.github.com>
2023-12-18 10:56:50 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     scripts=['bin/dswx_hls.py',
     	     'bin/dswx_compare.py'],
     install_requires=['argparse', 'numpy', 'yamale',
-                      'osgeo', 'scipy', 'pytest', 'requests'],
+                      'scipy', 'pytest', 'requests'],
     url='https://github.com/opera-adt/PROTEUS',
     license='Copyright by the California Institute of Technology.'
     ' ALL RIGHTS RESERVED.',
