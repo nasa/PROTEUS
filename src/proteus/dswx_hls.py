7849ed7184d052439e018993c72fe9d203dad1f8
Gustavo H. X. Shiroma <gustavo.h.shiroma@jpl.nasa.gov>
2025-03-05 15:28:27 -0800

    Add license statements to the DSWx-HLS metadata (5)

--- src/proteus/dswx_hls.py
+++ src/proteus/dswx_hls.py
@@ -822,7 +822,8 @@ def _compare_dswx_hls_metadata(metadata_1, metadata_2):
                 break
             # Exclude metadata fields that are not required to be the same
             if k1 in ['PROCESSING_DATETIME', 'DEM_SOURCE', 'LANDCOVER_SOURCE',
-                      'WORLDCOVER_SOURCE', 'SOFTWARE_VERSION', 'SENSOR']:
+                      'WORLDCOVER_SOURCE', 'SOFTWARE_VERSION', 'SENSOR',
+                      'LICENSE']:
                 continue
             if metadata_2[k1] != v1:
                 flag_same_metadata = False
